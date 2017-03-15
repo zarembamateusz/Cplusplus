dcb79b4d6387dda506d5f366c9b16921a23eb7c9
Mateusz <matteuszz20@gmail.com>
2017-03-15 22:51:39 +0100

    Mała poprawka

--- lab2/polybius/main.cpp
+++ lab2/polybius/main.cpp
@@ -6,17 +6,17 @@
 #include <iostream>
 #include "Polybius.h"
 #include <fstream>
-using std::ifstream
-using std::ofstream
-using std::cout
-using std::endl
-using std::ios_base
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::endl;
+using std::ios_base;
 using std::string;
 
 
 int main(int argc, char const *argv[]) {
 
-    if (argv[3] == "1") {
+    if (*argv[3] == '1') {
         ifstream myfile1(argv[1]);
         ofstream myfile2(argv[2], ios_base::in | ios_base::app);
         char word[64];
@@ -28,18 +28,20 @@ int main(int argc, char const *argv[]) {
         myfile1.close();
         myfile2.close();
     }
-    if (argv[3] == "0") {
-        ifstream myfile1("argv[1]");
-        ofstream myfile2("argv[2]", ios_base::in | ios_base::app);
+    if (*argv[3] == '0') {
+        ifstream myfile1(argv[1]);
+        ofstream myfile2(argv[2], ios_base::in | ios_base::app);
         char word[64];
         if (!myfile1)
             cout << "Nie można otworzyć pliku!" << endl;
         while (myfile1 >> word) {
+            //cout<<"raz "<<endl;
             myfile2 << PolybiusDecrypt(word);
         }
         myfile1.close();
         myfile2.close();
-    } else {
+    }
+    if(*argv[3]!='1' && *argv[3]!='0') {
         cout << "Podano bledny argument " << endl;
     }
     return 0;
