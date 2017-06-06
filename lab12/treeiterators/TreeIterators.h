ab3ac09b5d3c2d2161ecc726320142b2ea781a8c
Mateusz <matteuszz20@gmail.com>
2017-06-06 11:17:08 +0200

    Tree Iterators test works !

--- lab12/treeiterators/TreeIterators.h
+++ lab12/treeiterators/TreeIterators.h
@@ -7,7 +7,6 @@
 #include <memory>
 #include <iostream>
 #include <vector>
-
 namespace tree{
     template<class T>
     class Tree {
@@ -15,68 +14,43 @@ namespace tree{
         Tree();
         Tree(T value);
         void Insert(T value);
-        T Value();
-        std::shared_ptr<Tree> Left();
-        std::shared_ptr<Tree> Right();
-        int Size();
-        Tree<T> Root() ;
+        T Value(){return value_;};
+        int Size(){return size_;};
+        auto Root(){ return this;} ;
         size_t Depth();
-        std::shared_ptr<Tree> left_;
-        std::shared_ptr<Tree> right_;
+        std::unique_ptr<Tree> left_;
+        std::unique_ptr<Tree> right_;
         int size_;
         int depth_;
         T value_;
 
-    private:
-
     };
-
     template<class T>
     Tree<T>::Tree():size_(0),depth_(0) {
         value_ = 0;
         left_ = nullptr;
         right_ = nullptr;
     }
-
     template<class T>
     Tree<T>::Tree(T value):size_(1),depth_(1){
         value_ = value;
         left_ = nullptr;
         right_ = nullptr;
     }
-
     template<class T>
     void Tree<T>::Insert(T value) {
         if (value > value_) {
             if (!right_)
-                right_ = std::make_shared<Tree<T>>(value);
+                right_ = std::make_unique<Tree<T>>(value);
             else
                 right_->Insert(value);
         }else {
             if (!left_)
-                left_ = std::make_shared<Tree<T>>(value);
+                left_ = std::make_unique<Tree<T>>(value);
             else
                 left_->Insert(value);
         }
         size_++;
-    }
-    //get_value
-    template<class T>
-    T Tree<T>::Value(){
-        return value_;
-    }
-    template<class T>
-    std::shared_ptr<Tree<T>> Tree<T>::Left() {
-        return left_;
-    }
-    template<class T>
-    std::shared_ptr<Tree<T>> Tree<T>::Right(){
-        return right_;
-    }
-
-    template<class T>
-    int Tree<T>::Size(){
-        return size_;
     }
 
     template <class T>
@@ -90,84 +64,148 @@ namespace tree{
                 return 1+right_->Depth();
             return 1;
         }
-
-    }
-
-    template<class T>
-    Tree<T> Tree<T>::Root(){
-        Tree<int> tmp (0);
-        tmp.left_=left_;
-        tmp.right_=right_;
-        tmp.size_=size_;
-        tmp.depth_=depth_;
-        return *this;
     }
 
     template<class T>
     class InOrderTreeIterator{
     public:
-        InOrderTreeIterator(T value);
-        InOrderTreeIterator(Tree<int> &value);
-        void operator++();
-        int operator*();
-        int *iterator_;
-        void PrintTreeInOrder(std::shared_ptr<Tree<T>> uniq);
+        InOrderTreeIterator(Tree<int> *value): number_(0){PrintTree(value);};
+        void operator++(){number_+=1;};
+        int operator*(){return iterator_vector[number_];};
+        bool operator!=(const InOrderTreeIterator &sec)const{
+            return (iterator_vector!=sec.iterator_vector||number_!=sec.number_);
+        }
+        std::vector<int> Vector(){return iterator_vector;}
+        void SedIterator(int new_value){number_=new_value;};
+        void PrintTree(Tree<T> *uniq);
+
     private:
         std::vector<int> iterator_vector;
         int number_;
-
     };
-//złeee
 
     template<class T>
-    InOrderTreeIterator<T>::InOrderTreeIterator(T value):number_(0) {
-        PrintTreeInOrder(*std::make_unique<Tree<T>>(value));
-        *iterator_=1;
-
-    };
-    template<class T>
-    InOrderTreeIterator<T>::InOrderTreeIterator(Tree<int> &value):number_(0) {
-        //iterator_vector.push_back(int(value.Value()));
-        PrintTreeInOrder(std::make_shared<Tree<T>>(value));
-        *iterator_=iterator_vector[number_];
-    }
-
-    template<class T>
-    void InOrderTreeIterator<T>::PrintTreeInOrder(std::shared_ptr<Tree<T>> uniq) {
+    void InOrderTreeIterator<T>::PrintTree(Tree<T> *uniq) {
         if (uniq != nullptr) {
-            PrintTreeInOrder(uniq->Left());
+            PrintTree(uniq->left_.get());
             iterator_vector.push_back(int(uniq->Value()));
-            PrintTreeInOrder(uniq->Right());
+            PrintTree(uniq->right_.get());
         }
     }
 
-    template<class T>
-    void InOrderTreeIterator<T>::operator++() {
-
-        *iterator_=iterator_vector[number_+1];
-    }
-    template <class T>
-    int InOrderTreeIterator<T>::operator*() {
-        return *iterator_;
-    }
-
-    bool operator!=(const InOrderTreeIterator<int> &fir_arg, const InOrderTreeIterator<int> &sec_arg) {
-        bool n=true;
-        if(*fir_arg.iterator_!=*sec_arg.iterator_)
-            n=true;
-        else
-            n= false;
-        return n;
-    }
     template <class T>
     class InOrderTreeView{
     public:
-
+        InOrderTreeView(Tree<T> *tree) : i_(tree->Root()) {};
+        InOrderTreeIterator<T> begin() {
+            i_.SedIterator(0);
+            return i_;
+        }
+        InOrderTreeIterator<T> end() {
+            i_.SedIterator(i_.Vector().size()-1);
+            return i_;
+        }
+    private:
+        InOrderTreeIterator<T> i_;
+    };
+    template<typename T>
+    InOrderTreeView<T> InOrder(Tree<T> *value) {
+        return InOrderTreeView<T>(value);
     };
 
+    template<class T>
+    class PreOrderTreeIterator{
+    public:
+        PreOrderTreeIterator(Tree<T> *value): number_(0){PrintTree(value);};
+        void operator++(){number_+=1;};
+        T operator*(){return iterator_vector[number_];};
+        bool operator!=(const PreOrderTreeIterator &sec)const{
+            return (iterator_vector!=sec.iterator_vector||number_!=sec.number_);
+        }
+        std::vector<T> Vector(){return iterator_vector;}
+        void SedIterator(int new_value){number_=new_value;};
+        void PrintTree(Tree<T> *uniq);
 
+    private:
+        std::vector<T> iterator_vector;
+        int number_;
+    };
 
+    template<class T>
+    void PreOrderTreeIterator<T>::PrintTree(Tree<T> *uniq) {
+        if (uniq != nullptr) {
+            iterator_vector.push_back(uniq->Value());
+            PrintTree(uniq->left_.get());
+            PrintTree(uniq->right_.get());
+        }
+    }
 
+    template <class T>
+    class PreOrderTreeView{
+    public:
+        PreOrderTreeView(Tree<T> *tree) : i_(tree->Root()) {};
+        PreOrderTreeIterator<T> begin() {
+            i_.SedIterator(0);
+            return i_;
+        }
+        PreOrderTreeIterator<T> end() {
+            i_.SedIterator(i_.Vector().size()-1);
+            return i_;
+        }
+    private:
+        PreOrderTreeIterator<T> i_;
+    };
+    template<typename T>
+    PreOrderTreeView<T> PreOrder(Tree<T> *tree) {
+        return PreOrderTreeView<T>(tree);
+    };
+    template<class T>
+    class PostOrderTreeIterator{
+    public:
+        PostOrderTreeIterator(Tree<T> *value): number_(0){PrintTree(value);};
+        void operator++(){number_+=1;};
+        T operator*(){return iterator_vector[number_];};
+        bool operator!=(const PostOrderTreeIterator &sec)const{
+            return (iterator_vector!=sec.iterator_vector||number_!=sec.number_);
+        }
+        std::vector<T> Vector(){return iterator_vector;}
+        void SedIterator(int new_value){number_=new_value;};
+        void PrintTree(Tree<T> *uniq);
+
+    private:
+        std::vector<T> iterator_vector;
+        int number_;
+    };
+    template<class T>
+    void PostOrderTreeIterator<T>::PrintTree(Tree<T> *uniq) {
+        if (uniq != nullptr) {
+
+            PrintTree(uniq->left_.get());
+            PrintTree(uniq->right_.get());
+            iterator_vector.push_back(uniq->Value());
+        }
+    }
+
+    template <class T>
+    class PostOrderTreeView{
+    public:
+        PostOrderTreeView(Tree<T> *tree) : i_(tree->Root()) {};
+        PostOrderTreeIterator<T> begin() {
+            i_.SedIterator(0);
+            return i_;
+        }
+        PostOrderTreeIterator<T> end() {
+            i_.SedIterator(i_.Vector().size()-1);
+            return i_;
+        }
+
+    private:
+        PostOrderTreeIterator<T> i_;
+    };
+    template<typename T>
+    PostOrderTreeView<T> PostOrder(Tree<T> *tree) {
+        return PostOrderTreeView<T>(tree);
+    };
 
 }
 
